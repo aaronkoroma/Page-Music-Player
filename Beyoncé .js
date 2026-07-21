710dc1bbf0174794cf5916cf8f5a99d87b92bf52
Aaron Koroma <aaronkoroma357@gmail.com>
2026-07-21 12:51:38 +0000

    Add files via upload

---
+++ Beyoncé .js
@@ -0,0 +1,15 @@
+    function left() {
+      document.getElementById("image").src = "beyonce_mix.webp";
+      document.getElementById("title").innerHTML = "Mix";
+      document.getElementById("artist").innerHTML = "Beyoncé";
+      document.getElementById("song").src = "beyonce.mp3";
+      document.getElementById("copy").href = "Beyoncé - Halo.html";
+    }
+    function right() {
+      document.getElementById("image").src = "beyonce_texas.webp";
+      document.getElementById("title").innerHTML = "TEXAS HOLD 'EM";
+      document.getElementById("artist").innerHTML = "Beyoncé";
+      document.getElementById("song").src =
+        "[SPOTIFY-DOWNLOADER.COM] TEXAS HOLD 'EM.mp3";
+      document.getElementById("copy").href = "Beyoncé - Halo.html";
+    }
---
+++ Lukas.js
@@ -0,0 +1,15 @@
+    function left() {
+      document.getElementById("image").src = "Spirit Lead Me.png";
+      document.getElementById("title").innerHTML = "Spirit Lead Me";
+      document.getElementById("artist").innerHTML = "Hillsong United";
+      document.getElementById("song").src =
+        "Hillsong United - Spirit Leads Me (Lyrics) - Lunio Mix Music (youtube).mp3";
+      document.getElementById("copy").href = "Spirit Leads Me.html";
+    }
+    function right(){
+        document.getElementById("image").src = "doobie.webp";
+        document.getElementById("title").innerHTML = "The Doobie Brothers";
+        document.getElementById("artist").innerHTML = "Lukas Graham";
+        document.getElementById("song").src = "The_Doobie_Brothers_vs._Lukas_Graham_-_Years_Runnin_(mp3.pm).mp3";
+        document.getElementById("copy").href = "Lukas Graham.html";
+    }
