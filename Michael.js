a2b89e8ee36ed3251ffff21875c27b3d6f058639
Aaron Koroma <aaronkoroma357@gmail.com>
2026-07-21 12:45:59 +0000

    Add files via upload

---
+++ Michael.js
@@ -0,0 +1,14 @@
+    function left() {
+      document.getElementById("image").src = "Barbie Girl.png";
+      document.getElementById("title").innerHTML = "Barbie Girl";
+      document.getElementById("artist").innerHTML = "Aqua";
+      document.getElementById("song").src = "Barbie Girl - Aqua.mp3";
+      document.getElementById("copy").href = "Barbie Girl - Aqua.html";
+    }
+    function right() {
+      document.getElementById("image").src = "michael_jackson.webp";
+      document.getElementById("title").innerHTML = "The Way You Make Me Feel";
+      document.getElementById("artist").innerHTML = "Michael Jackson";
+      document.getElementById("song").src = "Michael_Jackson_-_The_Way_You_Make_Me_Feel_seks_momenty_(mp3.pm).mp3";
+      document.getElementById("copy").href = "the way you make me feel lyrics.html";
+    }
---
+++ main.js
@@ -0,0 +1,15 @@
+    function left() {
+      document.getElementById("image").src = "Barbie Girl.png";
+      document.getElementById("title").innerHTML = "Barbie Girl";
+      document.getElementById("artist").innerHTML = "Aqua";
+      document.getElementById("song").src = "Barbie Girl - Aqua.mp3";
+      document.getElementById("copy").href = "Barbie Girl - Aqua.html";
+    }
+    function right() {
+      document.getElementById("image").src = "Spirit Lead Me.png";
+      document.getElementById("title").innerHTML = "Spirit Lead Me";
+      document.getElementById("artist").innerHTML = "Hillsong United";
+      document.getElementById("song").src =
+        "Hillsong United - Spirit Leads Me (Lyrics) - Lunio Mix Music (youtube).mp3";
+      document.getElementById("copy").href = "Spirit Leads Me.html";
+    }
